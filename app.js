fd96795911228a1332a4b86c2d3a75f31c0793e0
Samuel Tuoyo <150522328+samueltuoyo@users.noreply.github.com>
2024-07-22 09:44:37 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const searchInput = document.getElementById('search-fetch-input');
 const searchBtn = document.getElementById('search-fetch-button');
 const weatherTemp = document.getElementById('weather-temp');
 const weatherDesc = document.getElementById('weather-description');
-const weatherIcon = document.querySelector('#weather-icon');
+const weatherIcon = document.getElementById('weather-icon');
+const WeatherHumidity = document.getElementById('humidity');
+const WindSpeed = document.getElementById('wind-speed');
 const apiKey = 'bf09142fa4854344916123406242007';
 
 function fetchWeather() {
@@ -14,15 +16,26 @@ function fetchWeather() {
     .then(data => {
       const temp = data.current.temp_c;
       const description = data.current.condition.text;
+      const humidity = data.current.humidity;
+      const windSpeed = data.current.wind_mph;
 
       if (description.includes('rain') || temp < 15) {
         weatherIcon.src = 'img/storm.png';
-      } else {
+      } 
+      
+      else if(description.includes('rain') || temp >= 15){
+          weatherIcon.src = 'img/sunny.png';
+      }
+      
+      else {
         weatherIcon.src = 'img/cloud.png';
       }
 
       weatherTemp.innerHTML = `${temp}°C`;
       weatherDesc.innerHTML = description;
+      WeatherHumidity.innerHTML = humidity + '%';
+      WindSpeed.innerHTML = windSpeed + 'Km/H';
+      
     })
     .catch(error => console.error(error));
 
